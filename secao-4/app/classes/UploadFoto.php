1da9adff6a232aac94ab19f27d46fbdaf9f4de9c
Cauã R. Pereira <7aauac@gmail.com>
2024-06-17 17:33:34 -0300

    Concluir aula 117

--- secao-4/app/classes/UploadFoto.php
+++ secao-4/app/classes/UploadFoto.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace app\classes;
+
 class UploadFoto
 {
 	public $file;
--- secao-4/index.php
+++ secao-4/index.php
@@ -1,10 +1,11 @@
 <?php
 
-require "app/classes/Pessoa.php";
-require "app/classes/AtividadesPessoa.php";
-require "app/classes/UploadFoto.php";
+require "vendor/autoload.php";
+
+use app\classes\UploadFoto;
 
 $upload = new UploadFoto;
+
 $upload->file('foto.png');
 $upload->extension();
 $upload->rename();
